a6d52e90b1ffe84412386d188a1661dd25f70f7e
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-01-19 23:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const inputbox = document.getElementById("inputbox");
+const licont = document.getElementById("licont");
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    showsaved();
+    setTimeout(()=>{
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    },3000);
+}); 
+
+
+inputbox.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addtask();
+    }
+});
+
+function addtask() {
+    if (inputbox.value.trim() === "") {
+        alert("Please write something");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = inputbox.value;
+
+    let span = document.createElement("span");
+    span.textContent = "\u00d7";
+
+    li.appendChild(span);
+    licont.appendChild(li);
+
+    inputbox.value = "";
+    save();
+}
+
+licont.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        save();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        save();
+    }
+});
+
+function save() {
+    localStorage.setItem("data", licont.innerHTML);
+}
+
+function showsaved() {
+    licont.innerHTML = localStorage.getItem("data") || "";
+}
+
+document.addEventListener("DOMContentLoaded", showsaved);
